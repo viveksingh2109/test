7ed338f073abc084cbd72a4d0c78ebd5c42ec04a
Vivek Singh <vivek6767singh6767@gmail.com>
2022-12-27 19:29:48 +0530

    function

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-n = int(input("Enter number: "))
-ans = 1
-for i in range(1,n+1):
-    ans*=i
-print(ans)
+def factorial():
+    n = int(input("Enter number: "))
+    ans = 1
+    for i in range(1,n+1):
+        ans*=i
+    print(ans)
+    return ans
